3ef8c096ab120548ad9b98bdd76d80b4a5e13aa9
baggiiiie <dave23eeeeem@gmail.com>
2022-11-02 18:01:09 +0800

    Updated seat checking logic

--- movie_page/seats.js
+++ movie_page/seats.js
@@ -1,5 +1,6 @@
-    var counter = 0;
-    var test = "1,1,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,0,0,1,1,1,0,1,1,0";
+    var arr_counter = 0;
+    var seat_counter = 0;
+    var test = "1,3,5,7,8,9,10,30,31,32,39,20,2";
     var test_arr = JSON.parse("[" + test + "]");
 
     for (row = 0; row < 5; row++) {
@@ -9,14 +10,15 @@
             }
             // if array_val == 1, print a box
             // if array_val == 0, print a checkbox
-            if (test_arr[counter] == 1) {
-                document.write("<input type=\"checkbox\" onclick=\"return false;\" class=\"taken_seat\" checked value=\""+counter+"\">");
+            if (test_arr.includes(seat_counter)) {
+                document.write("<input type=\"checkbox\" onclick=\"return false;\" class=\"taken_seat\" checked value=\""+arr_counter+"\">");
                 document.write("  <span></span>");
+                arr_counter++;
             } else {
-                document.write("<input type=\"checkbox\" class=\"free_seat\" value=\""+counter+"\">");
+                document.write("<input type=\"checkbox\" class=\"free_seat\" value=\""+arr_counter+"\">");
                 document.write("  <span></span>");
             }
-            counter ++;
+            seat_counter ++;
         }
         document.write("<br>");
     }
